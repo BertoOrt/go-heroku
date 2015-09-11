c5b03ff55b0c4df1a2451202fe5ed3013de9cbad
Roberto Ortega <berto.ort@gmail.com>
2015-09-11 14:52:37 -0600

    port

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"this": "works"})
 	})
-	r.Run(":5000")
+	r.Run(":" + port)
 }
